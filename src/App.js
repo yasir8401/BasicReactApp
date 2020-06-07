969371cab18d5eb970dd337d46e1f383b9b4a47c
yasir8401 <yasirnisar07@gmail.com>
2020-06-07 09:00:16 +0300

    Update Index.html

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Hello From Yasir Nisar in First React Project</p>
+        <p>Hello From Yasir Nisar and Adnan Hamdani in First React Project</p>
         <a
           className="App-link"
           href="https://reactjs.org"
